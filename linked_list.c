016838c879e587141171b7e30dc93d6aad00f502
Manav Gagvani <mgagvani@outlook.com>
2025-04-03 18:36:14 -0400

    finish hw10

--- linked_list.c
+++ linked_list.c
@@ -2,17 +2,85 @@
 
 #include <stdlib.h>
 
-struct list_node *new_node(size_t value) { return NULL; }
+struct list_node *new_node(size_t value) { 
+  struct list_node* new_node = malloc(sizeof(struct list_node));
+  new_node->value = value;
+  new_node->next = NULL;
+  return new_node;
+ }
 
-void insert_at_head(struct linked_list *list, size_t value) {}
+void insert_at_head(struct linked_list *list, size_t value) {
+  struct list_node* head = list->head;
+  struct list_node* new_head = new_node(value);
+  new_head->next = head; // reassign
+  list->head = new_head; // now make this the new head;
+}
 
-void insert_at_tail(struct linked_list *list, size_t value) {}
+void insert_at_tail(struct linked_list *list, size_t value) {
+  // create a new list if its empty
+  if (list->head == NULL) {
+    list->head = new_node(value);
+    return;
+  }
 
-size_t remove_from_head(struct linked_list *list) { return 0; }
+  // find the end
+  struct list_node* node = list->head;
+  while (node->next != NULL) {
+    node = node->next;
+  }
+  // now we have the last node.
+  node->next = new_node(value);
+}
 
-size_t remove_from_tail(struct linked_list *list) { return 0; }
+size_t remove_from_head(struct linked_list *list) { 
+  if (list->head == NULL) {
+    return 0; // empty list
+  }
+  
+  struct list_node* old_head = list->head;
+  size_t old_value = old_head->value;
+  list->head = old_head->next;
+  free(old_head);
+  return old_value;
+}
 
-void free_list(struct linked_list list) {}
+size_t remove_from_tail(struct linked_list *list) { 
+  if(list->head == NULL) {
+    return 0; // there wasn't anything there to begin with
+  }
+
+  // Only one node in the list
+  if(list->head->next == NULL) {
+    size_t value = list->head->value;
+    free(list->head);
+    list->head = NULL;
+    return value;
+  }
+
+  struct list_node* node = list->head;
+  struct list_node* prev_node = NULL;
+  while(node->next != NULL) {
+    prev_node = node;
+    node = node->next;
+  }
+  size_t to_ret = node->value;
+  free(node);
+  prev_node->next = NULL;
+  return to_ret;
+}
+
+void free_list(struct linked_list list) {
+  struct list_node* curr = list.head;
+  if (curr == NULL) {
+    return; // Empty list
+  }
+  
+  while (curr != NULL) {
+    struct list_node* next = curr->next;
+    free(curr);
+    curr = next;
+  }
+}
 
 // Utility function to help you debugging, do not modify
 void dump_list(FILE *fp, struct linked_list list) {
--- queue.c
+++ queue.c
@@ -1,8 +1,146 @@
 #include "queue.h"
+#include <stdlib.h>
+#include <string.h>
 #include "tile_game.h"
 
-void enqueue(struct queue *q, struct game_state state) {}
+/**
+ * Enqueue: Add to tail
+ * Dequeue: Remove from head
+ */
 
-struct game_state dequeue(struct queue *q) { return (struct game_state){0}; }
+void enqueue(struct queue *q, struct game_state state) {
+    insert_at_tail(&(q->data), serialize(state));
+}
 
-int number_of_moves(struct game_state start) { return 0; }
+struct game_state dequeue(struct queue *q) { 
+    size_t serialized_state = remove_from_head(&(q->data));
+    return deserialize(serialized_state);
+}
+
+bool in(size_t* arr, size_t* max_idx, size_t val) {
+    // is val in arr? search until max_idx
+    for(int i=0; i<*max_idx; i++) {
+        if (arr[i] == val) {
+            return true;
+        }
+    }
+    return false;
+}
+
+void get_children(struct queue* q, struct game_state curr, size_t* visited, size_t* max_idx) {
+    /**
+     * enqueues up, down, left, right children of curr to q
+     */
+    // make four copies of curr
+    struct game_state* currU = malloc(sizeof(struct game_state));
+    struct game_state* currD = malloc(sizeof(struct game_state));
+    struct game_state* currL = malloc(sizeof(struct game_state));
+    struct game_state* currR = malloc(sizeof(struct game_state));
+    *currU = curr;
+    *currD = curr;
+    *currL = curr;
+    *currR = curr;
+
+    size_t serialized;
+    
+    // only try valid moves.
+    if (curr.empty_row < 3) { 
+        move_up(currU);
+        // perform the reverse operation of the bit shift that adds the depth of transversal.
+        serialized = serialize(*currU) >> 15; 
+        if (!in(visited, max_idx, serialized)) { // if we haven't seen this node before.
+            enqueue(q, *currU);
+            visited[*max_idx] = serialized;
+            (*max_idx)++;
+        }
+    }
+    
+    if (curr.empty_row > 0) { 
+        move_down(currD);
+        serialized = serialize(*currD)  >> 15; 
+        if (!in(visited, max_idx, serialized)) { // if we haven't seen this node before.
+            enqueue(q, *currD);
+            visited[*max_idx] = serialized;
+            (*max_idx)++;
+        }
+    }
+    
+    if (curr.empty_col < 3) { 
+        move_left(currL);
+        serialized = serialize(*currL) >> 15; 
+        if (!in(visited, max_idx, serialized)) { // if we haven't seen this node before.
+            enqueue(q, *currL);
+            visited[*max_idx] = serialized;
+            (*max_idx)++;
+        }
+    }
+    
+    if (curr.empty_col > 0) { 
+        move_right(currR);
+        serialized = serialize(*currR) >> 15; 
+        if (!in(visited, max_idx, serialized)) { // if we haven't seen this node before.
+            enqueue(q, *currR);
+            visited[*max_idx] = serialized;
+            (*max_idx)++;
+        }
+    }
+    
+    // free the pointers, since we enqueued the literal values
+    free(currU);
+    free(currD);
+    free(currL);
+    free(currR);
+}
+
+bool goal_test(struct game_state state) {
+    // true if goal, false if no. 
+    // instaed of checking just empty cell, see if the 
+    // numbers match up.
+    for (int i = 0; i < 4; i++) {
+        for (int j = 0; j < 4; j++) {
+            if (i == 3 && j == 3) {
+                if (state.tiles[3][3] != 0) {
+                    return false;
+                }
+            } 
+            else if (state.tiles[i][j] != (i * 4 + j + 1)) {
+                return false;
+            }
+        }
+    }
+    return true;
+}
+
+int number_of_moves(struct game_state start) { 
+    struct queue* q = malloc(sizeof(struct queue));
+    // Initialize the queue
+    q->data.head = NULL;
+    enqueue(q, start);
+
+    struct game_state curr;
+
+    // i mean there are 15! states but lets pretend there are less
+    size_t* visited = calloc(1000000, sizeof(size_t));
+    size_t iter = 0; // how far along in visited have we got? 
+
+    while(q->data.head != NULL) {
+        printf("%ld\n", iter);
+        curr = dequeue(q);
+        if (goal_test(curr)) {
+            // Free the queue before returning
+            free_list(q->data);
+            free(visited);
+            free(q);
+            return curr.num_steps;
+        }
+        // add UDLR to queue
+        get_children(q, curr, visited, &iter);
+    }
+
+    // Free the queue before returning
+    free_list(q->data);
+    free(q);
+    // free visited
+    free(visited);
+    return 0;
+}
